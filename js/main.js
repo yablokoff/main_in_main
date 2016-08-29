ea168638bfe5a1123d6220fb2fadc5cc772a12a4
Alekseeva Olga <o.alekseeva@corp.mail.ru>
2016-08-30 02:28:27 +0300

    page 404, popup, edit article

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
 $( function() {
+	var $body = $('body');
+
+//form file
+	(function(){
+		var $fileBox = $(".js-form-file");
+
+		$fileBox.on ("change", ".js-form-file-input", (function(){
+			var $input = $(this),
+				$filePathBox = $input.closest(".js-form-file").find(".js-form-file-name"),
+				value = $input.val() ? $input.val() : $filePathBox.attr("data-empty")
+			;
+
+			$filePathBox.text(value);
+
+		}));
+
+	})();
+
+
+//popup
+	(function(){
+
+		$body.on ("click", ".js-popup-door", (function(event){
+			var $door = $(this),
+				id = $door.attr("data-popup"),
+				$popups = $(".js-popup"),
+				$curPopup = $body.find(".js-popup" + "[ data-popup="+ id +"]")
+			;
+
+			$popups.hide();
+			$curPopup.show();
+		}));
+
+		//close
+		$body.on ("click", ".js-popup-close", (function(){
+			var $popup = $(this).closest(".js-popup");
+
+			$popup.hide();
+		}));
+
+	})();
 
 });
